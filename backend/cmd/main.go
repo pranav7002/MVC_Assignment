0147f532dbd4088f72eca56251dca733f402adc0
pranav2007 <raipranav71@gmail.com>
2026-06-10 03:31:07 +0530

    add db connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"log/slog"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := godotenv.Load(); err != nil {
+		slog.Error("No .env file found")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	dbDSN := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", 
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
 	cfg := config{
-		addr: ":8080",
-		db: dbConfig{},
+		addr: fmt.Sprintf(":%s", port),
+		db: dbConfig{
+			dsn: dbDSN,
+		},
 	}
 
 	api := application{
 		config: cfg,
+		dbpool: nil,
 	}
 
+	pool, err := api.connectDB()
+	if err != nil {
+		slog.Error("Unable to create connection pool", "error", err)
+		os.Exit(1)
+	}
+	api.dbpool = pool
+	defer pool.Close() 
+
 	if err := api.run(api.mount()); err != nil {
-		log.Printf("server has fialed to start, with err: %s", err)
+		slog.Error("server failed to start", "error", err)
 		os.Exit(1)
 	}
 }
