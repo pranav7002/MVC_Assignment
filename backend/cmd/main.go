292a7ce0370872d54a6b38ebfb7ddfd022175268
pranav2007 <raipranav71@gmail.com>
2026-06-09 20:03:47 +0530

    add config files

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+    "log"
+    "net/http"
+
+    "github.com/go-chi/chi/v5"
+)
+
 func main() {
-	
+
+    r := chi.NewRouter()
+
+    log.Println("running on :8080")
+
+    http.ListenAndServe(":8080", r)
 }
